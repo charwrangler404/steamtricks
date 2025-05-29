02603d22f594e9bf2db6646e8864f3791f9acbaf
charwrangler404 <charwrangler404@gmail.com>
2025-05-28 21:49:47 -0500

    Update webinstall.sh

--- webinstall.sh
+++ webinstall.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-case "$(echo $SHELL | awk -F '/' '{print $NF}')" in
+SHELLNAME=$(echo $SHELL | awk -F '/' '{print $NF}')
+case "$SHELLNAME" in
   sh) echo "Shell not supported"; exit 1
   ;;
-  bash | zsh) curl https://raw.githubusercontent.com/charwrangler404/steamtricks/refs/heads/main/multiple_install_manager.sh >> "${HOME}/.${1}rc"; echo "Installed to ${HOME}/.${1}rc"
+  bash | zsh) curl https://raw.githubusercontent.com/charwrangler404/steamtricks/refs/heads/main/multiple_install_manager.sh >> "${HOME}/.${SHELLNAME}rc"; echo "Installed to ${HOME}/.${SHELLNAME}rc"
   ;;
   *) echo "You will need to install this by manually adding it to either your /etc/profile for system-wide installation or to your shell's profile"
   ;;
