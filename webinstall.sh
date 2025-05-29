8c9c96805d2e1f9f48213411e2c1088b45f5a0b7
charwrangler404 <charwrangler404@gmail.com>
2025-05-28 21:26:04 -0500

    Create webinstall.sh

---
+++ webinstall.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+install_manager () {
+  case "$1" in
+    sh) echo "Shell not supported"; exit 1
+    ;;
+    bash | zsh) curl https://github.com/charwrangler404/steamtricks/blob/3556747c57025d82af90929c7bd74c22e648e9d7/multiple_install_manager.sh >> "${HOME}/.${1}rc"
+    ;;
+    *) echo "You will need to install this by manually adding it to either your /etc/profile for system-wide installation or to your shell's profile"
+    ;;
+  esac
+)
+
+echo "Would you like to install the multiple-install game manager?"
+echo -n "[y/N]: "
+read ANS
+
+case "$ANS" in
+  y* | Y* ) install_manager $(echo $SHELL | awk -F "/" '{print $NF}')
+  ;;
+  *) echo "Aborting with no change"; exit 1
+  ;;
+esac
