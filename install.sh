f9d6e2d8db2a0577520118945701c9511ed0b8c3
charwrangler404 <charwrangler404@gmail.com>
2025-05-28 20:39:21 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+install_manager () {
+  case "$1" in
+    sh) echo "Shell not supported"; exit 1
+    ;;
+    bash | zsh) cat multiple_install_manager.sh | tee -a "${HOME}/.${1}rc"
+    ;;
+    *) echo "You will need to install this by manually adding it to either your /etc/profile for system-wide installation or to your shell's profile"
+    ;;
+  esac
+)
+
+echo "Would you like to install the multiple-install game manager?"
+echo -n "[y/N]: "
+read ANS
+
+case "$ANS" in
+  y* | Y* ) install_manager $(echo $SHELL | awk -F "/" '{print $NF}')
+  ;;
+  *) echo "Aborting with no change"; exit 1
+  ;;
+esac
